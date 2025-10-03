7274bbecd7c6b78f005ccd545409e4257a811b25
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-10-03 10:20:33 +0000

    add: favorite events to dashboard

--- src/app/administration/dashboard/dashboard.component.ts
+++ src/app/administration/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { User } from 'src/app/types/auth.types';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,6 @@ import { Component } from '@angular/core';
   standalone: false
 })
 export class DashboardComponent {
+  readonly #users = inject(UsersService);
+  user: User = this.#users.user()!;
 }
