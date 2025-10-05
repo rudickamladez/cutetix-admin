0c0495292cf66a5f5c00784512e2b25c5e3d938d
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-10-05 08:34:54 +0000

    add: copyright

---
+++ src/app/components/copyright/copyright.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+    selector: 'app-copyright',
+    templateUrl: './copyright.component.html',
+    styleUrls: ['./copyright.component.scss'],
+    standalone: false,
+})
+export class CopyrightComponent {
+    feelingIcon = faHeart;
+    current_date = new Date();
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserInfoComponent } from '../components/user-info/component';
+import { CopyrightComponent } from '../components/copyright/copyright.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
-    declarations: [UserInfoComponent],
-    imports: [CommonModule],
-    exports: [UserInfoComponent],
+    declarations: [
+        UserInfoComponent,
+        CopyrightComponent,
+    ],
+    imports: [
+        CommonModule,
+        FontAwesomeModule,
+    ],
+    exports: [
+        UserInfoComponent,
+        CopyrightComponent,
+    ],
 })
 export class SharedModule { }
