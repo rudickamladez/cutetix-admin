bf6210ad017b104eb918de0ac5a71a8266f9c206
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-10-13 15:44:54 +0000

    add: lint

---
+++ .prettierrc.ts
@@ -0,0 +1,19 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  endOfLine: "lf",
+  htmlWhitespaceSensitivity: "css",
+  objectWrap: "preserve",
+  quoteProps: "consistent",
+  semi: true,
+  singleAttributePerLine: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "es5",
+  useTabs: false,
+};
+
+export default config;
