8544331d36e27df62ca19ad9a933aeddbbae4e63
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2025-11-01 11:26:59 +0000

    fix: some sass lint problems

--- src/app/administration/dashboard/dashboard.component.ts
+++ src/app/administration/dashboard/dashboard.component.ts
@@ -3,7 +3,6 @@ import { Component } from "@angular/core";
 @Component({
   selector: "app-dashboard",
   templateUrl: "./dashboard.component.html",
-  styleUrls: ["./dashboard.component.scss"],
   standalone: false,
 })
 export class DashboardComponent {}
