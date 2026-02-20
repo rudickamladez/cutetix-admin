55728d6a424e133443a6a7968c67e9cddf1b8b33
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2026-02-20 14:20:43 +0100

    fix: rename to get_access_token_expiration
    Co-authored-by: Filip Zukal <130696213+sy-filis@users.noreply.github.com>

--- src/app/components/user-info/user-info.component.ts
+++ src/app/components/user-info/user-info.component.ts
@@ -88,7 +88,7 @@ export class UserInfoComponent implements OnInit, OnDestroy {
     ).format(new Date(exp * 1000));
   }
 
-  protected access_token_expire() {
+  protected get_access_token_expiration() {
     const exp = this.authService.getDecodedAccessToken()?.exp;
     if (!exp) return 'undefined';
     return this.#formatTokenExpirationDate(exp);
