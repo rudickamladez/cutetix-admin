83d717a4c3f54bb76c705bdc05fdd413a85e40f5
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2026-03-01 14:51:38 +0000

    add: change navbar menu due to scopes

--- src/app/administration/layout/nav-bar/menu-builder.ts
+++ src/app/administration/layout/nav-bar/menu-builder.ts
@@ -1,17 +1,29 @@
 import { faCalendarDays, faClock, faTicket } from '@fortawesome/free-solid-svg-icons';
 import { MenuItem, MenuSubItem } from './menu-items';
 import { IconDefinition } from '@fortawesome/angular-fontawesome';
+import { inject } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 export class MenuBuilder {
-    constructor() { }
+    readonly #auth = inject(AuthService);
+    #defaultMenu: MenuItem[] = [
+        this.#getMenuItem('My events', 'events', faCalendarDays, () => this.getSubItemsByPath('events')),
+    ].filter(o => o);
 
     build(): MenuItem[] {
+        let scopes = this.#auth.getScopes();
+        let username = this.#auth.getUsername();
+        if (scopes.includes("admin") || username.includes("admin") /*|| username.includes("matuska")*/) {
+            // user is admin and should have access to all things
+            return [
+                this.#getMenuItem('Tickets', 'tickets', faTicket, () => this.getSubItemsByPath('tickets')),
+                this.#getMenuItem('Ticket groups', 'ticket_groups', faClock, () => this.getSubItemsByPath('ticket_groups')),
+                this.#getMenuItem('Events', 'events', faCalendarDays, () => this.getSubItemsByPath('events')),
+            ].filter(o => o);
+        }
 
-        return [
-            this.#getMenuItem('Tickets', 'tickets', faTicket, () => this.getTicketsSubItems()),
-            this.#getMenuItem('Ticket groups', 'ticket_groups', faClock, () => this.getSubItemsByPath('ticket_groups')),
-            this.#getMenuItem('Events', 'events', faCalendarDays, () => this.getSubItemsByPath('events')),
-        ].filter(o => o);
+        // user has restricted access
+        return this.#defaultMenu;
     }
 
     getSubItemsByPath(
@@ -20,13 +32,6 @@ export class MenuBuilder {
         const result: MenuSubItem[] = [];
         result.push(new MenuSubItem('List', `/${path}/list`));
         result.push(new MenuSubItem('New', `/${path}/add`));
-        return result;
-    }
-
-    getTicketsSubItems(): MenuSubItem[] {
-        const result: MenuSubItem[] = [];
-        result.push(new MenuSubItem('List', '/tickets/list'));
-        result.push(new MenuSubItem('New', '/tickets/add'));
         return result;
     }
 
