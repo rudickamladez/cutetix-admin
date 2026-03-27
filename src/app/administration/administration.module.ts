c859382a8a497842f3a34c0e9e49ebee699da8d7
Lukáš Matuška <matuska.lukas@lukasmatuska.cz>
2026-03-27 20:28:00 +0000

    refactor: replace ToastrModule with ToastNoAnimationModule in AdministrationModule

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -8,8 +8,7 @@ import { NavBarSubitemComponent } from './layout/nav-bar-subitem/nav-bar-subitem
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { ToastrModule } from 'ngx-toastr';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastNoAnimationModule } from 'ngx-toastr';
 import { LoadingComponent } from './loading/loading.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TicketGroupsListComponent } from './ticket_groups/list/ticket_groups-list.component';
@@ -44,8 +43,7 @@ import { DashboardEventOverviewComponent } from './dashboard/event-overview/dash
         CommonModule,
         RouterModule,
         FontAwesomeModule,
-        BrowserAnimationsModule,
-        ToastrModule.forRoot(),
+        ToastNoAnimationModule.forRoot(),
         ReactiveFormsModule,
         SharedModule,
     ]
